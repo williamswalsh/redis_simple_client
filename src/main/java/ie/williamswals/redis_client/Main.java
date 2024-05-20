5c81ddac4f94b13ff9aa1f3dfb49fb7f7299a2ac
William Walsh <william.stephen.walsh@gmail.com>
2024-05-20 21:09:13 +1000

    Add programs to communicate with local redis instance.

--- src/main/java/ie/williamswals/redis_client/Main.java
+++ src/main/java/ie/williamswals/redis_client/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+package ie.williamswals.redis_client;
+
+import redis.clients.jedis.*;
+
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a Jedis connection pool
+        JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), "localhost", 6379);
+
+        // Get the pool and use the database
+        try (Jedis jedis = jedisPool.getResource()) {
+
+            jedis.set("mykey", "Hello from Redis");
+            String value = jedis.get("mykey");
+            System.out.println( value );
+
+            jedis.zadd("vehicles", 0, "car");
+            jedis.zadd("vehicles", 0, "bike");
+            List<String> vehicles = jedis.zrange("vehicles", 0, -1);
+            System.out.println( vehicles );
+        }
+
+        jedisPool.close();
+    }
 }
--- src/main/java/ie/williamswals/redis_client/Main2.java
+++ src/main/java/ie/williamswals/redis_client/Main2.java
@@ -1,2 +1,33 @@
-package ie.williamswals.redis_client;public class Main2 {
+package ie.williamswals.redis_client;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        try (Jedis jedis = new JedisPool("localhost", 6379).getResource()) {
+
+            jedis.set("foo", "bar");
+            System.out.println(jedis.get("foo")); // prints bar
+
+            Map<String, String> hash = new HashMap<>();
+            hash.put("name", "John");
+            hash.put("surname", "Smith");
+            hash.put("company", "Redis");
+            hash.put("age", "29");
+            jedis.hset("user-session:123", hash);
+            Map<String,String> retrieved = jedis.hgetAll("user-session:123");
+
+            System.out.println("First Name: " + retrieved.get("name"));
+            System.out.println("Surname: " + retrieved.get("surname"));
+            System.out.println("Company: " + retrieved.get("company"));
+            System.out.println("Age: " + retrieved.get("age"));
+
+            // Prints: {name=John, surname=Smith, company=Redis, age=29}
+        }
+    }
 }
